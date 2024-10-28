4ef2f4a2901bad61d9b7e96d4babf63bdc20ff4e
Jeff Tao <jatao88@gmail.com>
2024-10-28 13:31:54 -0400

    Rudimentary XdY syntax

--- src/dicelang/dice.test.ts
+++ src/dicelang/dice.test.ts
@@ -1,7 +1,51 @@
-import { expect, test } from "vitest";
-import { diceGrammar } from "./dice";
+import { expect, test, describe } from "vitest";
+import { diceGrammar, diceSemantics } from "./dice";
+import { test as fctest, fc } from "@fast-check/vitest";
 
 test("parses a dice roll", () => {
   const match = diceGrammar.match("2d6");
   expect(match.succeeded()).toBe(true);
+});
+
+const MAX_DICE_COUNT = 1000;
+
+describe("dice semantics", () => {
+  fctest.prop([
+    fc.integer({ min: 1, max: MAX_DICE_COUNT }),
+    fc.integer({ min: 1 }),
+  ])("XdY should return a number between Y and X * Y", (dCount, dSize) => {
+    const match = diceGrammar.match(`${dCount}d${dSize}`);
+    const result = diceSemantics(match).eval();
+    return result >= dCount && result <= dCount * dSize;
+  });
+
+  fctest.prop([fc.nat(MAX_DICE_COUNT)])("Xd0 should return 0", (dCount) => {
+    const match = diceGrammar.match(`${dCount}d0`);
+    const result = diceSemantics(match).eval();
+    return result === 0;
+  });
+
+  fctest.prop([fc.nat()])("0dY should return 0", (dSize) => {
+    const match = diceGrammar.match(`0d${dSize}`);
+    const result = diceSemantics(match).eval();
+    return result === 0;
+  });
+
+  fctest.prop([fc.integer({ max: -1 }), fc.integer({ min: 1 })])(
+    "XdY should return 0 when X is negative",
+    (dCount, dSize) => {
+      const match = diceGrammar.match(`${dCount}d${dSize}`);
+      const result = diceSemantics(match).eval();
+      return result === 0;
+    }
+  );
+
+  fctest.prop([fc.integer({ max: -1 }), fc.integer({ min: 1 })])(
+    "XdY should return 0 when Y is negative",
+    (dCount, dSize) => {
+      const match = diceGrammar.match(`${dCount}d${dSize}`);
+      const result = diceSemantics(match).eval();
+      return result === 0;
+    }
+  );
 });
--- src/dicelang/dice.ts
+++ src/dicelang/dice.ts
@@ -1,10 +1,30 @@
 import * as ohm from "ohm-js";
+import { randomInteger, range, sum } from "remeda";
 
-export const diceGrammar = ohm.grammar(`
+export const diceGrammar = ohm.grammar(String.raw`
   DiceLang {
     Program = Roll
     Roll = Number "d" Number
 
-    Number = digit+
+    Number = "-"? digit+
   }
 `);
+
+export const diceSemantics = diceGrammar
+  .createSemantics()
+  .addOperation("eval", {
+    Roll: (num1, _, num2) => {
+      const dCount = Math.max(num1.eval(), 0);
+      const dSize = Math.max(num2.eval(), 0);
+
+      if (dSize === 0) {
+        return 0;
+      }
+
+      return sum(range(0, dCount).map(() => randomInteger(1, dSize)));
+    },
+
+    Number: (sign, digits) => {
+      return parseInt(sign.sourceString + digits.sourceString);
+    },
+  });
